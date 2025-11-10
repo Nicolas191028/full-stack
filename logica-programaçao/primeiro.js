c4336d9089e550bc34ff9c2db0c2f20f645c80bf
Nicolas191028 <du.antunes@icloud.com>
2025-11-10 15:07:40 -0300

    Criando index.html , primeiro.js e segundo.js

---
+++ logica-programaçao/primeiro.js
@@ -0,0 +1,11 @@
+// Perguntnado o nome para o usuário
+const userName = prompt("Qual é seu nome?");
+
+// Perguntando qual é sua idade
+const userAge = prompt("Qual é sua idade?");
+
+// Imprimindo na telo o resultado
+document.write("Seu nome é: " + "<strong>" + userName + "</strong>" + "<br>");
+
+// Imprimindo na tela a idade
+document.write(" e sua idade é: " + "<strong>" + userAge + " anos" + "</strong>");
