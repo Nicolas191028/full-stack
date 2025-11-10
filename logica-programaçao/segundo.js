6b4730953cf8d0e3f1ae475b65b55405eccdb6a4
Nicolas191028 <du.antunes@icloud.com>
2025-11-10 15:41:23 -0300

    teste

--- logica-programaçao/segundo.js
+++ logica-programaçao/segundo.js
@@ -1,4 +1,4 @@
-let numero1 = prompt("Digite um número");
-let numero2 = prompt("Digite outro número");
+let numero1 = Number(prompt("Digite um número"));
+let numero2 = Number(prompt("Digite outro número"));
 
 console.log(numero1 + numero2)
