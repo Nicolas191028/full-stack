7c517b8e75083c35521c898bb4482d164b83e383
Nicolas191028 <du.antunes@icloud.com>
2025-11-13 16:13:10 -0300

    Criando/Reaprendnedo for

---
+++ logica-programaçao/nomes.js
@@ -0,0 +1,8 @@
+const names = ["B2", "Rodrigo", "Victoria", "Dudu", "Alemão", "Carlos"];
+//               0       1           2         3       4
+
+// length
+
+for (i = 0; i < names.length; i++){
+    console.log("✈️ 🏢💥🏢 " + names[i])
+}
