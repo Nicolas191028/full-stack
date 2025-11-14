f0604ace86f85658ef94cef38493511312e26a93
Nicolas191028 <du.antunes@icloud.com>
2025-11-14 15:03:16 -0300

    Aprendendo for e while

---
+++ logica-programaçao/times.js
@@ -0,0 +1,17 @@
+/*
+Crie esse array de times 
+const times = ["Botafogo", "São Paulo", "Paulista"];
+
+Usando for loop, mostre na tela, a messagem seguinte:
+- O time é: Botafogo
+- O time é: São Paulo
+- O time é: Paulista
+*/
+
+const times = ["Botafogo", "São Paulo", "Paulista"];
+//                  0           1           2
+
+for (i = 0; i < times.length; i++){
+    document.write("- O time é:" + times[i] + "<br>")
+}
+
