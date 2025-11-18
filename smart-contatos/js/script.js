d980aa082f01f9f43f2123286c6fc8830c96f1b7
Nicolas191028 <du.antunes@icloud.com>
2025-11-18 16:15:22 -0300

    Adicionando li

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form");
+const lista = document.querySelector(".lista")
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTelefone = document.getElementById("telefone");
@@ -6,12 +7,27 @@ const inputTelefone = document.getElementById("telefone");
 form.addEventListener("submit", function(event) {
     event.preventDefault(); 
     
-    if (inputNome.value === "" || inputEmail.value === "" ||inputTelefone.value === "" ) {
+    if (inputNome.value == "" || inputEmail.value == "" || inputTelefone.value == "" ) {
         alert("Digite todos os campos");
+
         return false;
     }
 
-    console.log("Nome" , inputNome.value);
-    console.log("Email" , inputEmail.value);
-    console.log("Telefone" , inputTelefone.value);
+    // Criar LI
+    const li = document.createElement("li")
+
+    li.innerHTML = `
+        <span class = "contato-nome">${inputNome.value}</span>
+        <span class = "contato-email">${inputEmail.value}</span>
+        <span class = "contato-telefone">${inputTelefone.value}</span>
+    `;
+
+    console.log(li)
+
+
+    // apendChild
+    lista.appendChild(li)
+
+    // Limpar inputs
+    form.reset()
 });
