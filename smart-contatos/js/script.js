97cd805aaad12ad5310be0a301a565ebd35545eb
Nicolas191028 <du.antunes@icloud.com>
2025-11-18 15:02:10 -0300

    Adicionando lógica para espaços em branco

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,10 +1,17 @@
+const form = document.querySelector("form");
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTelefone = document.getElementById("telefone");
 
-function btnCadastrar(event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault(); 
-    console.log(inputNome.value);
-    console.log(inputEmail.value);
-    console.log(inputTelefone.value);
-}
+    
+    if (inputNome.value === "" || inputEmail.value === "" ||inputTelefone.value === "" ) {
+        alert("Digite todos os campos");
+        return false;
+    }
+
+    console.log("Nome" , inputNome.value);
+    console.log("Email" , inputEmail.value);
+    console.log("Telefone" , inputTelefone.value);
+});
