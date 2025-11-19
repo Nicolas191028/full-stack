bf4016c1f57c95d9376a19bcf4730aa145f26964
Nicolas191028 <du.antunes@icloud.com>
2025-11-19 16:01:39 -0300

    Adicionando endereço e programando excluir

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -3,13 +3,14 @@ const lista = document.querySelector(".lista")
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTelefone = document.getElementById("telefone");
+const inputEndereco = document.getElementById("endereco");
 const listaMsg = document.querySelector(".lista-msg");
 
 
 form.addEventListener("submit", function(event) {
     event.preventDefault(); 
     
-    if (inputNome.value == "" || inputEmail.value == "" || inputTelefone.value == "" ) {
+    if (inputNome.value == "" || inputEmail.value == "" || inputTelefone.value == "" || inputEndereco.value == "") {
         alert("Digite todos os campos");
 
         return false;
@@ -27,17 +28,22 @@ form.addEventListener("submit", function(event) {
     btnExcluir.className = "btn-delete";
 
     // Criar LI
-    const li = document.createElement("li")
+    const li = document.createElement("li");
 
     // Criando uma função para excluir 
     btnExcluir.addEventListener("click", function() {
-        alert("test")
+        const confirmar = confirm("Tem certeza que deseja excluir o contato?");
+
+        if (confirmar) {
+            li.remove();
+        }
     })
 
     li.innerHTML = `
         <span class = "contato-nome">${inputNome.value}</span>
         <span class = "contato-email">${inputEmail.value}</span>
         <span class = "contato-telefone">${inputTelefone.value}</span>
+        <span class = "contato-telefone">${inputEndereco.value}</span>
     `;
 
     console.log(li)
