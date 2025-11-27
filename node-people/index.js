80ed36cfd65adbff009e12432f33d986c0afb6ab
Nicolas191028 <du.antunes@icloud.com>
2025-11-27 16:22:57 -0300

    Criando novas rotas para times

--- node-people/index.js
+++ node-people/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000; // Executar na porta 3000
 
+// Indicar para express ler body com json
+app.use(express.json());
+
 // mock
 const nomes = [
   { id: 1, nome: "Fernanda", idade: "18" },
@@ -11,15 +14,33 @@ const nomes = [
   { id: 5, nome: "Doris", idade: "33" },
 ];
 
+const times = [
+  { id: 1, nome: "Corinthians", estado: "SP", titulos: 7 },
+  { id: 2, nome: "Palmeiras", estado: "SP", titulos: 11 },
+  { id: 3, nome: "Santos", estado: "SP", titulos: 8 },
+  { id: 4, nome: "Flamengo", estado: "RJ", titulos: 7 },
+  { id: 5, nome: "Vasco", estado: "RJ", titulos: 4 },
+  { id: 6, nome: "Atlético Mineiro", estado: "MG", titulos: 3 },
+  { id: 7, nome: "Cruzeiro", estado: "MG", titulos: 4 },
+];
+
 // Criando Funções Auxiliares
 // Retornar o objeto por Id
 function buscarNomePorId(id) {
     return nomes.filter((nome) => nome.id == id)
 }
 
+function buscarTimePorId(id) {
+    return times.filter((time) => time.id == id)
+}
+
 // Pegar a posição ou index do elemento do Array por id
 function buscarIdNomes(id) {
     return nomes.findIndex((nome) => nome.id == id)
+}
+
+function buscarIdTimes(id) {
+    return times.findIndex((time) => time.id == id)
 }
 
 app.get("/", (req, res) => {
@@ -31,9 +52,13 @@ app.get("/teste", (req, res) => {
     res.send("API nodePeople está funcionando!");
 });
 
-// Buscando nomes (listaNomes)
+// Buscando nomes/times (listaNomes)/(times)
 app.get("/listaNomes", (req, res) => {
     res.send(nomes);
+});
+
+app.get("/times", (req, res) => {
+    res.send(times);
 });
 
 // Buscando por ID
@@ -43,10 +68,21 @@ app.get("/listaNomes/:id", (req, res) => {
     res.json(buscarNomePorId(index))
 });
 
+app.get("/times/:id", (req, res) => {
+    let index = req.params.id;
+
+    res.json(buscarTimePorId(index))
+});
+
 // Criando Post para cadastrar
 app.post("/listaNomes", (req, res) => {
     nomes.push(req.body);
-    res.status(201).send('Nomes cadastrado com sucesso');
+    res.status(201).send('Nome cadastrado com sucesso');
+});
+
+app.post("/times", (req, res) => {
+    times.push(req.body);
+    res.status(201).send('Time cadastrado com sucesso');
 });
 
 // Criando Rota Excluir
@@ -54,6 +90,12 @@ app.delete("/listaNomes/:id", (req, res) => {
     let index = buscarIdNomes(req.params.id);
     nomes.splice(index, 1);
     res.send(`Nomes com id ${req.params.id} excluida com sucesso`);
+});
+
+app.delete("/times/:id", (req, res) => {
+    let index = buscarIdTimes(req.params.id);
+    times.splice(index, 1);
+    res.send(`Times com id ${req.params.id} excluida com sucesso`);
 });
 
 app.listen(PORT, () => {
