ef9b5cf655381aa7c01ce226ba17d137fb8e1f1c
Nicolas191028 <du.antunes@icloud.com>
2025-12-02 15:40:47 -0300

    Criando arquivo de cadastro com rotas get,put,delete,post

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -56,4 +56,33 @@ app.get("/listaCadastros/:id", (req, res) => {
     res.json(buscarCadastroPorId(index))
 });
 
+app.post("/listaCadastros", (req, res) => {
+   cadastros.push(req.body)
+   res.status(201).send('Cadastrado com sucesso') 
+});
+
+app.delete("/listaCadastros/:id", (req, res) => {
+    let id = req.params.id;
+    let index = buscarIdCadastros(id)
+
+    if (index === -1) {
+        return res.status(404).send(`Nenhum cadastro com id ${id} encontrado`)
+    }
+
+    cadastros.splice(index, 1);
+    res.send(`Cadastro com id ${id} deletado com sucesso`)
+});
+
+app.put("/listaCadastros/:id", (req, res) => {
+    let index = buscarIdCadastros(req.params.id);
+    cadastros[index].nome = req.body.nome;
+    cadastros[index].telefone = req.body.telefone;
+    cadastros[index].cpf = req.body.cpf;
+    cadastros[index].email = req.body.email;
+    cadastros[index].idade = req.body.idade;
+    cadastros[index].endereco = req.body.endereco;
+
+    res.json(cadastros);
+});
+
 export default app;
